fe7547feec3f3e1ace4ba95fd752c3d0f54859f5
kangazoom <michellelcronin@gmail.com>
2019-05-25 23:30:46 -0700

    Built out queries

--- sf_open_food_trucks_app/api_wrapper.py
+++ sf_open_food_trucks_app/api_wrapper.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import requests
 from food_truck import FoodTruck
@@ -14,11 +15,40 @@ def get_open_food_truck_list():
         print('1: FOOD TRUCK + ADDRESS')
     parse_response(response)
 
+def current_date_time():
+    now = datetime.datetime.now()
+    # day_of_the_week = now.day
+    # hour = now.hour
+    # minute = now.minute
+    # second = now.second
+    # microsecondond = now.microsecond
+    return now
+
+def build_query_collection():
+    # queries
+    # select
+    now = current_date_time()
+    limit = str(20)
+    day_of_week_num = str(now.isoweekday())
+    sort_alpha_by = 'applicant'
+    current_time_as_string = '\'' + str(now.hour) + ':' + str(now.minute) + '\''
+    start_time_boundary = 'start24<=' +  current_time_as_string
+    end_time_boundary = 'end24>=' +  current_time_as_string
+    complete_time_boundary = start_time_boundary + ' and ' + end_time_boundary
+    query_string = (
+    '?' + '$limit=' + limit 
+    + '&' + 'dayorder=' + day_of_week_num
+    + '&' + '$order=' + sort_alpha_by
+    + '&' + '$where=' + complete_time_boundary)
+    return query_string
+    
+    
+
 
 def fetch_food_truck_data():
     base_url = 'https://data.sfgov.org/resource/jjew-r69b.json'
-    limit_query = str(20)
-    response = requests.get(base_url + '?' + '$limit=' + limit_query)
+    query_string = build_query_collection()
+    response = requests.get(base_url + query_string)
     return response
 
 
@@ -34,5 +64,9 @@ def parse_response(response):
             food_truck.start_time = item['start24']
             food_truck.end_time = item['end24']
             print(food_truck.name)
+            print(food_truck.address)
+            print(food_truck.day_open)
+            print(food_truck.start_time)
+            print(food_truck.end_time)
             open_food_truck_object_collection.append(food_truck)
     return open_food_truck_object_collection
